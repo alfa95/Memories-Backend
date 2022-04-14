424c0c8213e93a830de465dd2241877d4b5406c3
anus0719 <anurag.s@netcracker.com>
2022-04-15 00:31:06 +0530

    Integration of Login Through Google and Jwt

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,23 @@
-import express from 'express';
-import { getPosts, createPost, updatePost } from '../controllers/posts.js';
+import express from "express";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get("/", getPosts);
 
-router.post('/', createPost);
+router.post("/", auth, createPost);
 
-router.patch('/:id', updatePost);
+router.patch("/:id", auth, updatePost);
+
+router.delete("/:id", auth, deletePost);
+
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
